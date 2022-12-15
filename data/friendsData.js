140cbd806cf53afa7ce26050f5a8d4bc2766011f
Jiayang <91884683+UltramarineW@users.noreply.github.com>
2022-12-15 10:14:23 +0800

    Update friendsData.js

--- data/friendsData.js
+++ data/friendsData.js
@@ -1,9 +1,9 @@
 const friendsData = [
-  //   {
-  //     name: 'piggy',
-  //     url: 'https://piggy.moe/',
-  //     page: "Piggy's Home",
-  //   },
+     {
+       name: 'piggy',
+       url: 'https://piggy.moe/',
+       page: "Piggy's Home",
+     },
 ]
 
 export default friendsData
